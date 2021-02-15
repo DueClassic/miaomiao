41db6325670d287e32389ac8e1f8d57d7b58ccfe
chen.cheng <cheng.chen@percent.cn>
2021-02-15 21:52:05 +0800

    add setData

--- src/main.js
+++ src/main.js
@@ -1,12 +1,19 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 
-Vue.config.productionTip = false
+import axios from "axios";
+Vue.prototype.axios = axios;
+
+Vue.filter('setWH',(url,arg)=>{
+  return url.replace(/w\.h/,arg);
+})
+
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
